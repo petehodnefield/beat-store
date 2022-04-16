ad53f91dad37f43fdfe64aa8d3ec43dfb24c2744
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-04-16 10:28:19 -0500

    hamburger menu working well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-var iceMountain = document.querySelector("#ice-mountain-audio");
-var january = document.querySelector("#january-audio");
-var buyBtn = document.querySelector(".buy-btn")
+
 var dummyContainer = document.querySelector("#dummy-container")
 
 let hamburgerMenuEl = document.querySelector(".hamburger")
-let rotatedHamburgerMenuEl = document.querySelector(".rotated-hamburger")
 let navItemsContainerEl = document.querySelector(".nav-links-container")
 
-function playIceMountain() {
-    iceMountain.play();
-}
-function playJanuary() {
-    january.play();
-}
 
 let showMenuItems = function() {
-    console.log(navItemsContainerEl)
+    if(navItemsContainerEl.style.display === "block") {
+        navItemsContainerEl.style.display = "none"
+        hamburgerMenuEl.style.transform = 'rotate(180deg)';
+        hamburgerMenuEl.style.transition = '.5s';
+
+    }
+    else {
+        navItemsContainerEl.style.display = "block"
+        hamburgerMenuEl.style.transform = 'rotate(90deg)'
+        hamburgerMenuEl.style.transition = '.5s';
+
+    }
     navItemsContainerEl.classList.remove("hidden")
     hamburgerMenuEl.classList.add("rotated-hamburger")
 }
@@ -25,4 +27,3 @@ let hideMenuItems = function() {
 }
 
 hamburgerMenuEl.addEventListener("click", showMenuItems)
-rotatedHamburgerMenuEl.addEventListener("click", hideMenuItems)
